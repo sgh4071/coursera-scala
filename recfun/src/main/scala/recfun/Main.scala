d24d92027d97074582a3af2df1077266bed9f833
ubuntu-12.04 <yingliu@ubuntu.(none)>
2014-10-17 09:58:04 -0700

    first commit

---
+++ recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,38 @@
+package recfun
+import common._
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = {
+    val foo: Int = List(c, r) match {
+      case List(0, _) => 1
+      case List(_, 0) => 1
+      case _ => pascal(c-1, r-1) + pascal(c, r-1)
+    }
+    foo
+  }
+
+  
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = {
+    
+  }
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = 0
+}
